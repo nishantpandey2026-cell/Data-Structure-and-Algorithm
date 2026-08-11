44ad91a9f48ce853089ad6a0f75bbeeb4a6abf16
nishantpandey2026-cell <nishantpandey2026@gmail.com>
2026-08-11 19:38:11 +0530

    Time: 2 ms (99.34%) | Memory: 47.2 MB (23.71%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> set=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            int need=target-nums[i];
+            if(set.containsKey(need))
+            {
+                return new int[]{set.get(need),i};
+            }
+            set.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
