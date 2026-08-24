93f8e4507161461cdb4ab3b295c3d2fab3fa2305
nishantpandey2026-cell <nishantpandey2026@gmail.com>
2026-08-24 20:01:39 +0530

    Time: 80 ms (9.31%), Space: 106.8 MB (55.61%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer>stack=new Stack<>();
+        int res[]=new int[temperatures.length];
+        Arrays.fill(res,0);
+        for(int i=temperatures.length-1;i>=0;i--)
+        {
+            while(!stack.isEmpty() && temperatures[i]>=temperatures[stack.peek()])
+            {
+                stack.pop();
+            }
+            if(!stack.isEmpty())
+            {
+                res[i]=stack.peek()-i;
+            }
+            stack.push(i);
+            
+        }
+        return res;
+    }
+}
